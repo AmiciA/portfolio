8277062f630c061a7ae32e8e395f422787405e7f
Anthony Amici <amici93@gmail.com>
2017-11-03 10:52:56 +0800

    Add files via upload

---
+++ Honour/js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $( "#button-hk" ).click(function() {
+        $( "#hk" ).toggle();
+        $( "#hk-2" ).toggle();
+    });
+    
+    $( "#button-it" ).click(function() {
+        $( "#it" ).toggle();
+        $( "#it-2" ).toggle();
+    });
+});
